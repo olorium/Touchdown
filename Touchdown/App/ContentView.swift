264124a6a9b3236f24518df62ef83ce3d2ad10ea
Alex Vasyliev <olorin2007@gmail.com>
2023-04-10 14:03:20 +0200

    initial commit

---
+++ Touchdown/App/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Touchdown
+//
+//  Created by Oleksii Vasyliev on 10.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	// MARK: - Properties
+	
+	// MARK: - Body
+    var body: some View {
+		ZStack {
+			VStack(spacing: 0) {
+				NavigationBarView()
+					.padding(.horizontal, 15)
+					.padding(.bottom)
+					.padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+					.background(Color.white)
+					.shadow(color: .black.opacity(0.05), radius: 5, x: 0, y: 5)
+				Spacer()
+				FooterView()
+					.padding(.horizontal)
+			}
+			.background(Constants.colorBackground.ignoresSafeArea(.all, edges: .all))
+		}
+		.ignoresSafeArea(.all, edges: .top)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,38 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Oleksii Vasyliev on 10.04.2023.
+//
+
+import SwiftUI
+
+/// Constants for the project.
+@dynamicMemberLookup
+struct Constants {
+	// MARK: - Data
+	
+	// MARK: - Color
+	let colorBackground = Color("ColorBackground")
+	let colorGray = Color(UIColor.systemGray4)
+	// MARK: - Layout
+	
+	// MARK: - UX
+	
+	// MARK: - API
+	
+	// MARK: - Image
+	
+	// MARK: - Font
+	
+	// MARK: - Font
+	
+	// MARK: - Misc
+	
+	/// Prevent outside construction by making the init private.
+	private init() {}
+	
+	static subscript<T>(dynamicMember keyPath: KeyPath<Constants, T>) -> T {
+		Constants()[keyPath: keyPath]
+	}
+}
