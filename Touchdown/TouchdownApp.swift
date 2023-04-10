b1915245d0b57abdef7d9fcb14a5c28dc95bc974
Alex Vasyliev <olorin2007@gmail.com>
2023-04-10 12:55:36 +0200

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Oleksii Vasyliev on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
